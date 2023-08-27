7484d155ffa20734ad1c1c2ecac97b3977520cd5
Jonathan Smårs <jsmars@gmail.com>
2023-08-27 11:10:52 +0200

    Added project files for version 0.1

---
+++ source/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace DataSearcher
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnSearch = new System.Windows.Forms.Button();
+			this.txtInput = new System.Windows.Forms.TextBox();
+			this.txtOutput = new System.Windows.Forms.TextBox();
+			this.folderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+			this.txtPath = new System.Windows.Forms.TextBox();
+			this.btnBrowse = new System.Windows.Forms.Button();
+			this.txtFilter = new System.Windows.Forms.TextBox();
+			this.txtRight = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.btnHelp = new System.Windows.Forms.Button();
+			this.btnGithub = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btnSearch
+			// 
+			this.btnSearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnSearch.Location = new System.Drawing.Point(12, 92);
+			this.btnSearch.Name = "btnSearch";
+			this.btnSearch.Size = new System.Drawing.Size(281, 28);
+			this.btnSearch.TabIndex = 0;
+			this.btnSearch.Text = "Search";
+			this.btnSearch.UseVisualStyleBackColor = true;
+			this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+			// 
+			// txtInput
+			// 
+			this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtInput.Location = new System.Drawing.Point(12, 27);
+			this.txtInput.Name = "txtInput";
+			this.txtInput.Size = new System.Drawing.Size(281, 20);
+			this.txtInput.TabIndex = 1;
+			this.txtInput.Text = "altitude: ";
+			// 
+			// txtOutput
+			// 
+			this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtOutput.Location = new System.Drawing.Point(12, 126);
+			this.txtOutput.Multiline = true;
+			this.txtOutput.Name = "txtOutput";
+			this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.txtOutput.Size = new System.Drawing.Size(449, 474);
+			this.txtOutput.TabIndex = 2;
+			// 
+			// txtPath
+			// 
+			this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtPath.Location = new System.Drawing.Point(12, 66);
+			this.txtPath.Name = "txtPath";
+			this.txtPath.Size = new System.Drawing.Size(281, 20);
+			this.txtPath.TabIndex = 3;
+			// 
+			// btnBrowse
+			// 
+			this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnBrowse.Location = new System.Drawing.Point(383, 65);
+			this.btnBrowse.Name = "btnBrowse";
+			this.btnBrowse.Size = new System.Drawing.Size(78, 21);
+			this.btnBrowse.TabIndex = 4;
+			this.btnBrowse.Text = "Browse";
+			this.btnBrowse.UseVisualStyleBackColor = true;
+			this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+			// 
+			// txtFilter
+			// 
+			this.txtFilter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtFilter.Location = new System.Drawing.Point(299, 66);
+			this.txtFilter.Name = "txtFilter";
+			this.txtFilter.Size = new System.Drawing.Size(78, 20);
+			this.txtFilter.TabIndex = 5;
+			this.txtFilter.Text = "*.srt";
+			// 
+			// txtRight
+			// 
+			this.txtRight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtRight.Location = new System.Drawing.Point(299, 27);
+			this.txtRight.Name = "txtRight";
+			this.txtRight.Size = new System.Drawing.Size(162, 20);
+			this.txtRight.TabIndex = 7;
+			this.txtRight.Text = "]";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 50);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(32, 13);
+			this.label1.TabIndex = 8;
+			this.label1.Text = "Path:";
+			// 
+			// label2
+			// 
+			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(296, 50);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(81, 13);
+			this.label2.TabIndex = 9;
+			this.label2.Text = "Search Pattern:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 11);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(72, 13);
+			this.label3.TabIndex = 10;
+			this.label3.Text = "Search string:";
+			// 
+			// label4
+			// 
+			this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(296, 11);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(71, 13);
+			this.label4.TabIndex = 11;
+			this.label4.Text = "Ending string:";
+			// 
+			// btnHelp
+			// 
+			this.btnHelp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnHelp.Location = new System.Drawing.Point(383, 92);
+			this.btnHelp.Name = "btnHelp";
+			this.btnHelp.Size = new System.Drawing.Size(78, 28);
+			this.btnHelp.TabIndex = 13;
+			this.btnHelp.Text = "Help";
+			this.btnHelp.UseVisualStyleBackColor = true;
+			this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+			// 
+			// btnGithub
+			// 
+			this.btnGithub.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnGithub.Location = new System.Drawing.Point(299, 92);
+			this.btnGithub.Name = "btnGithub";
+			this.btnGithub.Size = new System.Drawing.Size(78, 28);
+			this.btnGithub.TabIndex = 14;
+			this.btnGithub.Text = "Github";
+			this.btnGithub.UseVisualStyleBackColor = true;
+			this.btnGithub.Click += new System.EventHandler(this.btnGithub_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(473, 612);
+			this.Controls.Add(this.btnGithub);
+			this.Controls.Add(this.btnHelp);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtRight);
+			this.Controls.Add(this.txtFilter);
+			this.Controls.Add(this.btnBrowse);
+			this.Controls.Add(this.txtPath);
+			this.Controls.Add(this.txtOutput);
+			this.Controls.Add(this.txtInput);
+			this.Controls.Add(this.btnSearch);
+			this.Name = "Form1";
+			this.Text = "GPS Altitude Finder version 0.1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnSearch;
+		private System.Windows.Forms.TextBox txtInput;
+		private System.Windows.Forms.TextBox txtOutput;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowser;
+		private System.Windows.Forms.TextBox txtPath;
+		private System.Windows.Forms.Button btnBrowse;
+		private System.Windows.Forms.TextBox txtFilter;
+		private System.Windows.Forms.TextBox txtRight;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button btnHelp;
+		private System.Windows.Forms.Button btnGithub;
+	}
+}
+
---
+++ source/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataSearcher
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnBrowse_Click(object sender, EventArgs e)
+		{
+			if (folderBrowser.ShowDialog() == DialogResult.OK)
+				txtPath.Text = folderBrowser.SelectedPath;
+		}
+
+		private void btnSearch_Click(object sender, EventArgs e)
+		{
+			if (txtPath.Text.Length == 0)
+			{
+				Log("No valid path chosen");
+				return;
+			}
+			var files = Directory.GetFiles(txtPath.Text, txtFilter.Text);
+
+			var data = new Dictionary<string, List<string>>();
+			var dataNumeric = new Dictionary<string, List<double>>();
+			var total = new List<double>();
+			int lineCount = 0;
+
+			foreach (var file in files)
+			{
+				var filename = Path.GetFileName(file);
+				var list = data[filename] = new List<string>();
+				var listNumeric = dataNumeric[filename] = new List<double>();
+
+				var lines = File.ReadAllLines(file);
+				lineCount += lines.Length;
+
+				foreach (var line in lines)
+				{
+					var index = line.IndexOf(txtInput.Text);
+					if (index >= 0)
+					{ 
+						var start = index + txtInput.Text.Length;
+						var end = line.IndexOf(txtRight.Text, start);
+						var dataStr = end < 0 ? line.Substring(start) : line.Substring(start, end - start);
+						list.Add(dataStr);
+						if (double.TryParse(dataStr, NumberStyles.Any, CultureInfo.InvariantCulture, out var val))
+						{
+							listNumeric.Add(val);
+							total.Add(val);
+						}
+					}
+				}
+			}
+
+			//foreach (var list in dataNumeric)
+			//	list.Value.Sort();
+			//total.Sort();
+
+			Log($"Files searched: {files.Length}");
+			Log($"Lines searched: {lineCount}");
+
+			if (total.Count == 0)
+			{
+				Log("No numeric data found in lines");
+				return;
+			}
+
+			Log($"Min: {total.Min()} Max: {total.Max()} Average: {total.Average()}");
+
+			double max = 0;
+			string maxFile = "";
+			var listDiff = new Dictionary<string, double>();
+			var minusFiles = new List<string>();
+			foreach (var list in dataNumeric)
+			{
+				var diff = list.Value.Max() - list.Value.Min();
+				listDiff[list.Key] = diff;
+				if (diff > max)
+				{
+					maxFile = list.Key;
+					max = diff;
+				}
+				if (list.Value.Any(x => x < 0))
+					minusFiles.Add(list.Key);
+			}
+
+			var sortedDict = from entry in listDiff orderby entry.Value descending select entry;
+
+			Log($"Max diff: {max} for {maxFile} ({(max * 10).ToString("0.0")} meters)");
+
+			foreach (var item in sortedDict)
+			{
+				var list = dataNumeric[item.Key];
+				Log($"{item.Key} Diff: {(item.Value * 10).ToString("0.00")} meters.    Min: {list.Min()} Max: {list.Max()} Average: {list.Average()} Diff: {list.Max() - list.Min()} {(minusFiles.Contains(item.Key) ? "Contains NEGATIVE altitude! Recording started in air. Check manually!" : "")}");
+			}
+
+			//foreach (var list in dataNumeric)
+			//	Log($"{list.Key} Min: {list.Value.Min()} Max: {list.Value.Max()} Average: {list.Value.Average()} Diff: {list.Value.Max() - list.Value.Min()}");
+		}
+
+		void Log(string text)
+		{
+			txtOutput.Text += text + "\r\n";
+		}
+
+		private void btnHelp_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Info: " +
+				"\nThis application was mainly made to search through a folder of GPS files created for DJI FPV and calculating the max altitude of the clips, but could in theory be used for other stuff as well.It will search though all files and show you the ranges of altitude values in the files.If I've understood things correctly, the altitude data in the DJI FPV is based on barometer data and might not be completely accurate, and also it will show the value divided by 10, and this is compensated for. In some cases the altitude is displayed as a negative value as well. The displayed height is the difference between the min and max altitude values multiplied by 10, so if you start or land in the video, and the barometer is working correctly, it should be fairly accurate, but check manually if you are unsure." +
+				"\n\nInstructions:" +
+				"\nBrowse to your directory and press the search button." +
+				"\n\nHope this is helpful! " +
+				"\n\nCheck out this project on github for any fixes etc." +
+				"\nCreated by Jonathan Smårs. jsmars.com") ;
+		}
+
+		private void btnGithub_Click(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start("https://github.com/jsmars/GPS-Altitude-Finder/");
+		}
+	}
+}
